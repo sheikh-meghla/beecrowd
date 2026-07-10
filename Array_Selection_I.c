dcccba07f3a844c8c5d3ec8ea4c68b8c74e99f20
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-10 16:07:54 +0600

    Add Array Selection I problem solution in C

---
+++ Array_Selection_I.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float A[100];
+
+    for (int i = 0; i < 100; i++)
+    {
+        scanf("%f", &A[i]);
+
+        if (A[i] <= 10.0)
+        {
+            printf("A[%d] = %.1f\n", i, A[i]);
+        }
+    }
+
+    return 0;
+}
