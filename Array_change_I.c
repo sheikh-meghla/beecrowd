25cd6fa1d07d35d36f35baa515f9d9b2fa618f6a
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-10 16:15:30 +0600

    Add Array Change I problem solution in C

---
+++ Array_change_I.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int N[20];
+    int temp;
+
+    for (int i = 0; i < 20; i++)
+    {
+        scanf("%d", &N[i]);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        temp = N[i];
+        N[i] = N[19 - i];
+        N[19 - i] = temp;
+    }
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("N[%d] = %d\n", i, N[i]);
+    }
+
+    return 0;
+}
+
