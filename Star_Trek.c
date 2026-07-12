acef79793559d4c328c549b34cfd552406704c1b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-12 19:56:20 +0600

    Add Star Trek problem solution in C

---
+++ Star_Trek.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    long long star[n];
+    int visited[n];
+
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &star[i]);
+        sum += star[i];
+        visited[i] = 0;
+    }
+
+    int attacked = 0;
+    int pos = 0;
+
+    while (pos >= 0 && pos < n) {
+
+        if (visited[pos] == 0) {
+            visited[pos] = 1;
+            attacked++;
+        }
+
+        long long sheep = star[pos];
+
+        if (star[pos] > 0) {
+            star[pos]--;
+            sum--;
+        }
+
+        if (sheep % 2 == 1)
+            pos++;
+        else
+            pos--;
+    }
+
+    printf("%d %lld\n", attacked, sum);
+
+    return 0;
+}
