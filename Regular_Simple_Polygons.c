221079fe4f004980fd777feb5e10378240f1088c
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-12 18:31:23 +0600

    Add Regular Simple Polygons problem solution in C

---
+++ Regular_Simple_Polygons.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+        long long int bahu;
+        long long int trivuj;
+        scanf("%lld %lld",&bahu,&trivuj);
+
+        long long int ans = bahu * trivuj;
+        printf("%lld\n",ans);
+    
+    return 0;
+}
