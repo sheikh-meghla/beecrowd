5c0b5dab85d4b0b010a7737849b8036952b9070b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-12 17:40:00 +0600

    Add Vitória and Her Indecision problem solution in C

---
+++ Vitória_and_Her_Indecision.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    char choise1[10];
+    char name1[100];
+    while (t--) {
+        scanf("%s %s", name1,choise1);
+        
+        
+    }
+    printf("Ciencia da Computacao\n");
+
+
+    return 0;
+}
