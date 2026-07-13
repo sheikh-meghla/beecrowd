f4d8693863b76e32d557fadb7e613df827de2c6f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-13 10:30:40 +0600

    Refactor MacPRONALTS solution for improved readability by adding braces to conditional statements

--- MacPRONALTS.c
+++ MacPRONALTS.c
@@ -13,15 +13,30 @@ int main()
         scanf("%d %d", &id, &q);
 
         if (id == 1001)
+        {
             total += 1.50 * q;
+        }
+            
         else if (id == 1002)
+        {
             total += 2.50 * q;
+        }
+            
         else if (id == 1003)
+        {
             total += 3.50 * q;
+        }
+            
         else if (id == 1004)
+        {
             total += 4.50 * q;
+        }
+            
         else if (id == 1005)
+        {
             total += 5.50 * q;
+        }
+            
     }
 
     printf("%.2f\n", total);
