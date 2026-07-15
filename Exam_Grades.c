c57233c38c96254430c20beebd2e00360cb75c74
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-15 13:25:37 +0600

    Add Exam Grades problem solution in C

---
+++ Exam_Grades.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n >= 86)
+    {
+        printf("A\n");
+    }
+    else if (n >= 61)
+    {
+        printf("B\n");
+    }
+    else if (n >= 36)
+    {
+        printf("C\n");
+    }
+    else if (n >= 1)
+    {
+        printf("D\n");
+    }
+    else
+    {
+        printf("E\n");
+    }
+
+    return 0;
+}
