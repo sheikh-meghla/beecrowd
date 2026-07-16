d15864d93d0589ed1e85605a0a6214ededd8af64
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-16 17:46:11 +0600

    Add duplicate number detection solution in C

---
+++ duplicate_num.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int freq[50] = {0};
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        freq[a[i]]++;
+    }
+    
+    for(int i=0;i<=50;i++)
+    {
+        if(freq[i] > 1)
+        {
+            printf("%d\n",i);
+        }
+    }
+    
+    
+    
+
+    return 0;
+}
