ad1bacd6c250759903356ad43a6cf7e912612966
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-20 15:55:15 +0600

    Add solution for Input and Output Character problem in C

---
+++ Input_and_Output_Character.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char x,y,z;
+    scanf("%c %c %c",&x,&y,&z);
+
+    printf("A = %c, B = %c, C = %c\n",x,y,z);
+    printf("A = %c, B = %c, C = %c\n",y,z,x);
+    printf("A = %c, B = %c, C = %c\n",z,x,y);
+
+
+    return 0;
+}
