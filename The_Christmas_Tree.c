ae1c2d49195462e57ddea367a5891a692d804643
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-23 12:54:26 +0600

    Add solution for The Christmas Tree problem in C

---
+++ The_Christmas_Tree.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    while (n--)
+    {
+        int h, d, g;
+        scanf("%d %d %d", &h, &d, &g);
+
+        if (h >= 200 && h <= 300 && d >= 50 && g >= 150)
+        {
+            printf("Sim\n");
+        }
+        else
+        {
+            printf("nao\n");
+        }
+    }
+
+    return 0;
+}
