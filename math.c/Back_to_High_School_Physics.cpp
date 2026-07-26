ecb48709de830a1f906fd9afd37e4c357cfefde7
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-26 19:10:49 +0600

    Add solution for Back to High School Physics problem in C++

---
+++ math.c/Back_to_High_School_Physics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int v,t;
+    while (cin >> v >> t)
+    {
+        
+        int ans = 2 * v * t;
+        cout << ans<<endl;
+    }
+    
+    return 0;
+} 
