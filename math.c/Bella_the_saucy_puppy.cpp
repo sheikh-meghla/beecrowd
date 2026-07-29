220f562b2942052aa037e20b0ff25e63434f9b0f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-29 17:24:25 +0600

    Add solution for Bella, the saucy puppy problem in C++

---
+++ math.c/Bella_the_saucy_puppy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n <= 3)
+    {
+        cout << "vai ganhar o biscoito\n";
+    }
+    else
+    {
+        cout << "vai ficar sem o biscoito\n";
+    }
+
+    return 0;
+}
