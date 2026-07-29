6a6a7fb953534775a9b095943e6567f8f4ac754f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-29 09:07:23 +0600

    Add solution for Etruscan Warriors Never Play Chess problem in C++

---
+++ math.c/Etruscan_Warriors_Never_Play_Chess.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int x = (-1 + (sqrt(1+8*n)))/2;          
+        cout << x << endl;
+ 
+        }
+
+        
+    }
+    
