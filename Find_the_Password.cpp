deeb83aa05096d602a4fe6bd67d42f153ce76d49
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-29 17:41:58 +0600

    Add solution for Find the Password problem in C++

---
+++ Find_the_Password.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int a,b,c,d;
+    while (cin >> a >> b >> c >> d)
+    {
+        cout << a <<b << c << d << endl;
+    }
+    
+    return 0;
+}
