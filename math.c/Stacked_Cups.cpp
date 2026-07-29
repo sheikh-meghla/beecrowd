d13ea421dde86ccf59ad28d98b05994279db0a1b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-29 17:27:40 +0600

    Add solution for Stacked Cups problem in C++

---
+++ math.c/Stacked_Cups.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    while (cin >> A >> B) {
+        if (B > A)
+        {
+            cout << "ok\n";
+        }
+        else
+        {
+            cout << "no\n";
+        }
+    }
+    return 0;
+}
