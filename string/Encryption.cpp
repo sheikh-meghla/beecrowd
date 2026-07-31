4937899bf4a951c11960c833a00fdd7a5639c3e4
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-31 20:40:48 +0600

    Add solution for Encryption problem in C++

---
+++ string/Encryption.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    while (n--) {
+        string s;
+        getline(cin, s);
+
+        for (int i = 0; i < s.size(); i++) {
+            if (isalpha(s[i])) {
+                s[i] += 3;
+            }
+        }
+
+        reverse(s.begin(), s.end());
+
+        for (int i = s.size() / 2; i < s.size(); i++) {
+            s[i]--;
+        }
+
+        cout << s << "\n";
+    }
+
+    return 0;
+}
