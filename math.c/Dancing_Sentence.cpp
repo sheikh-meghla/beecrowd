d3daed478055b1a9dcd289e2a2d841cf9ad67af4
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-31 20:30:57 +0600

    Add solution for Dancing Sentence problem in C++

---
+++ math.c/Dancing_Sentence.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+
+    while (getline(cin, s)) {
+        bool upper = true;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ')
+            {
+                continue;
+            }
+
+            if (upper)
+            {
+                s[i] = toupper(s[i]);
+            }
+            else
+            {
+                s[i] = tolower(s[i]);
+            }
+
+            upper = !upper;
+        }
+
+        cout << s << "\n";
+    }
+
+    return 0;
+}
