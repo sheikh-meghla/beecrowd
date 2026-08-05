40676a0402b26ae93295b863b8c7595a423b43ae
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-05 16:33:34 +0600

    Add solution for Making Kites problem in C++

---
+++ math.c/Making_Kites.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    while (N--) {
+        int x, y;
+        cin >> x >> y;
+
+        cout << (x * y) / 2 << " cm2\n";
+    }
+
+    return 0;
+}
