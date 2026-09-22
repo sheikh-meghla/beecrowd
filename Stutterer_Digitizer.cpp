ee3ea7ae2b7e971121549ec4d003702774929b2a
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-22 20:35:45 +0600

    Refactor output formatting in Stutterer Digitizer solution

--- Stutterer_Digitizer.cpp
+++ Stutterer_Digitizer.cpp
@@ -3,16 +3,25 @@ using namespace std;
 
 int main() {
     string s;
+    bool flag = true;
 
     while (cin >> s) {
+
         if (s.size() >= 4) {
             if (s[0] == s[2] && s[1] == s[3]) {
                 s.erase(0, 2);
             }
         }
 
-        cout << s << " ";
+        if (!flag) {
+            cout << " ";
+        }
+
+        cout << s;
+        flag = false;
     }
+
+    cout << endl;
 
     return 0;
 }
